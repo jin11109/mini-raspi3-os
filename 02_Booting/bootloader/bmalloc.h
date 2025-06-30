8f9b57cfea90ddbf7da70448c66712b2ab74222b
jin11109 <wuchejin@gmail.com>
2025-06-30 12:44:05 +0800

    Update to change name of include guard

--- 02_Booting/bootloader/bmalloc.h
+++ 02_Booting/bootloader/bmalloc.h
@@ -1,8 +1,8 @@
-#ifndef _BMALLOC_H
-#define _BMALLOC_H
+#ifndef _BOOTLOADER_BMALLOC_H
+#define _BOOTLOADER_BMALLOC_H
 
 #include "def.h"
 
 void* bmalloc(size_t size);
 
-#endif /*_BMALLOC_H*/
+#endif /*_BOOTLOADER_BMALLOC_H*/
